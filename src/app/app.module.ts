f85953dccab5cd8753ffd9eae3d73489eca4b27b
bsidy <bsidy01@gmail.com>
2021-08-26 11:42:55 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Exo1Component } from './exo1/exo1.component';
+import { Exo3Component } from './exo3/exo3.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Exo1Component,
+    Exo3Component
   ],
   imports: [
     BrowserModule,
---
+++ src/app/exo3/exo3.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-exo3',
+  templateUrl: './exo3.component.html',
+  styleUrls: ['./exo3.component.css']
+})
+export class Exo3Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
